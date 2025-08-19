d37f46fab5d7f7690824056ba1174caee469dde7
OldSchool299792458 <144597433+nigelgalbraith@users.noreply.github.com>
2025-08-19 15:55:06 +1200

    ..

--- projects/js/JSON/sketchfabModels.js
+++ projects/js/JSON/sketchfabModels.js
@@ -6,7 +6,7 @@ const SKETCHFAB_MODELS = {
     embedUrl: "https://sketchfab.com/models/f8835a039ccd44e09f831213514e628c/embed",
     directLink: "https://sketchfab.com/3d-models/terrace-gardens-f8835a039ccd44e09f831213514e628c",
     username: "nigelgalbraith",
-    userProfile: "https://sketchfab.com/Oldschool299792458",
+    userProfile: "https://sketchfab.com/nigelgalbraith",
     links: [
       {
         href: "https://sketchfab.com/3d-models/terrace-gardens-f8835a039ccd44e09f831213514e628c",
@@ -27,7 +27,7 @@ const SKETCHFAB_MODELS = {
     embedUrl: "https://sketchfab.com/models/04bd290ad70d402a8e07a7ce4ddbeacb/embed",
     directLink: "https://sketchfab.com/3d-models/greenhouse-planter-boxes-capping-04bd290ad70d402a8e07a7ce4ddbeacb",
     username: "nigelgalbraith",
-    userProfile: "https://sketchfab.com/Oldschool299792458",
+    userProfile: "https://sketchfab.com/nigelgalbraith",
     links: [
       {
         href: "https://sketchfab.com/3d-models/greenhouse-planter-boxes-capping-04bd290ad70d402a8e07a7ce4ddbeacb",
